4422311bb04461f85d6821c25b43acd396620b32
Firoz Kc <kcfiroz93@gmail.com>
2026-01-03 10:36:04 +0530

    Force TheLegacyReader as root application entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TheLegacyReader from './TheLegacyReader'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TheLegacyReader from "./TheLegacyReader.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <TheLegacyReader />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+export default TheLegacyReader;
